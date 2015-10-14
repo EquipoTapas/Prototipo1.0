1d6a4245f3264e70aacbbe45530cac5aab19db91
EquipoTapas <equipotapas@gmail.com>
2015-10-13 22:13:48 -0500

    Inserción de productos ficticios

--- dll/Tapas/www/js/controllers.js
+++ dll/Tapas/www/js/controllers.js
@@ -1,11 +1,19 @@
 angular.module("modulocliente.controllers", ["ionic"])
 
-.controller("contenido", function($scope, $ionicSideMenuDelegate, $firebaseArray){
-	
-  	//$scope.Productos.scroll = scrollRef.scroll;
+.controller("contenido", function($scope, $ionicSideMenuDelegate){
 
+	
+
+
+	var ob = [	{"id":"333", "imgproducto":"https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTQ_DaRnpmI36m5jkTO5XtDuYhcoBpAE5hkqOYA3k7Jv2MfRBY8zw","popularidad":"2"},
+				{"id":"222", "imgproducto":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCm_lecJMWbmXNV2zJmwyRFA6oxrh0lTNBW7oYWseapLvZ_sT9_g","popularidad":"23"},
+				{"id":"888", "imgproducto":"http://www.3cordilleras.com/images/3c_blanca.png","popularidad":"34"},
+				{"id":"111", "imgproducto":"http://www.exito.com/images/products/413/0000311465277413/0000311466835404_lrg_a.jpg","popularidad":"44"}]
+
+	$scope.Productos = ob;
 	$scope.popularidad = 0;
 	$scope.cantidad = 0;
+	$scope.imgproducto = "";
 
 	//Evento clic Menú Superior Izq
 	$scope.toggleLeft = function() {
